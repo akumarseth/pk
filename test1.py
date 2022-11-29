4f0b0b52edd49ffa589bf77f9f78441146b8d893
Abhishek Kumar Seth <abhishek.kumar1@tredence.com>
2022-11-30 00:31:01 +0530

    updated

--- test1.py
+++ test1.py
@@ -1,2 +1,31 @@
 print("from test1py file")
 print("from test1py file updated")
+
+money = 10.5
+name="prabhakr"
+
+count = 1
+
+def sum(x, y):
+    print(x+y)
+
+    global count
+    count += 1
+    print(count)
+
+    # if count >= 10:
+    #     pass
+    # else:
+    #     sum()
+
+    # if count < 10:
+    #     sum()
+
+
+sum(10, 20)
+
+
+# read csv/excel
+# Apply logic
+# return result 
+# send mail with result as a attachment ot in body
