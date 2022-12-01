51d6e52cda255fcb56deda5ca3504f6331f09efd
Abhishek Kumar Seth <abhishek.kumar1@tredence.com>
2022-12-01 23:02:11 +0530

    updated list and tuple

--- test1.py
+++ test1.py
@@ -29,3 +29,56 @@ sum(10, 20)
 # Apply logic
 # return result 
 # send mail with result as a attachment ot in body
+
+x = ['a','b','c','d']
+print(x[0])
+print(x[-1])
+print(x[len(x)-1])
+
+print(x[1:3])
+
+
+for data in x:
+    print(data)
+
+
+print("Line No 45")
+print("*****************")
+
+print(range(5))
+print(range(len(x)-1))
+
+for i in range(len(x)):
+    print(x[i])
+
+
+# append
+# extend
+
+print(list.__sizeof__(x))
+
+
+x.append('t')
+print(x)
+
+# print(list.__sizeof__(x))
+
+y = ['ram','sita']
+
+x.extend(y)
+print(x)
+print(list.__sizeof__(x))
+
+t = ('a','b','c')
+print(tuple.__sizeof__(t))
+
+t = ('a','x','d','d')
+print(tuple.__sizeof__(t))
+
+# tuple
+# set
+# dict
+
+
+
+
